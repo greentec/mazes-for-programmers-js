c7bcf8bed2a1df0a1c4f20fdb92c84a9cbda39ed
Damjan Pavlica <damjan.pavlica@origo.is>
2022-08-02 15:50:32 +0200

    refact

--- Ch11/ch11_01_prims_demo/main.js
+++ Ch11/ch11_01_prims_demo/main.js
@@ -1,10 +1,9 @@
-import { SimplifiedPrims } from '../../mazes/Prims.js'
+import { SimplifiedPrims } from '../../mazes/algorithms/Prims.js'
 import Grid from '../../mazes/Grid.js'
 
-const maze = new SimplifiedPrims()
 const grid = new Grid(20)
 const start_at = grid.random_cell
-maze.on(grid, start_at)
+SimplifiedPrims.on(grid, start_at)
 
 const start = grid.middle_cell
 grid.distances = start.distances
