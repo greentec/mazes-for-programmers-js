45498874938e60c7d4dca9696f60fe4110fa93f1
Damjan Pavlica <damjan.pavlica@origo.is>
2022-08-02 17:14:26 +0200

    pomalo

--- Ch12/ch12_01_ellers_demo/main.js
+++ Ch12/ch12_01_ellers_demo/main.js
@@ -3,7 +3,5 @@ import Ellers from '../../mazes/algorithms/Ellers.js'
 
 const grid = new Grid(20)
 Ellers.on(grid)
-// grid.braid(0.5);
 
 grid.drawDistance()
-
--- mazes/algorithms/Ellers.js
+++ mazes/algorithms/Ellers.js
@@ -1,5 +1,46 @@
-import RowState from '../RowState.js'
 import { shuffle } from '../utils.js'
+
+class RowState {
+  constructor(starting_set = 0) {
+    this.cells_in_set = {}
+    this.set_for_cell = []
+    this.next_set = starting_set
+  }
+
+  record(set, cell) {
+    this.set_for_cell[cell.column] = set
+
+    if (!this.cells_in_set.hasOwnProperty(set)) this.cells_in_set[set] = []
+    this.cells_in_set[set].push(cell)
+  }
+
+  set_for(cell) {
+    if (!this.set_for_cell[cell.column]) {
+      this.record(this.next_set, cell)
+      this.next_set += 1
+    }
+
+    return this.set_for_cell[cell.column]
+  }
+
+  merge(winner, loser) {
+    for (let i = 0; i < this.cells_in_set[loser].length; i += 1) {
+      const cell = this.cells_in_set[loser][i]
+      this.set_for_cell[cell.column] = winner
+      this.cells_in_set[winner].push(cell)
+    }
+    delete this.cells_in_set[loser]
+  }
+
+  next() {
+    return new RowState(this.next_set)
+  }
+
+  * each_set() {
+    for (const [set, cells] of Object.entries(this.cells_in_set))
+      yield [set, cells]
+  }
+}
 
 export default class Ellers {
   static on(grid) {
@@ -8,7 +49,6 @@ export default class Ellers {
     for (const row of grid.each_row()) {
       for (const cell of row) {
         if (!cell.west) continue
-
         const set = row_state.set_for(cell)
         const prior_set = row_state.set_for(cell.west)
 
@@ -21,24 +61,14 @@ export default class Ellers {
 
       if (row[0].south) {
         const next_row = row_state.next()
-        const each_set_gen = row_state.each_set()
-
-        while (true) {
-          const next = each_set_gen.next()
-          if (next.done == true) break
-          const [set, list] = next.value
-          if (!set) break
-          if (!list || list.length == 0) break
-
-          shuffle(list)
-
-          for (let index = 0; index < list.length; index += 1) {
-            const cell = list[index]
-            if (index == 0 || Math.random() < 0.33) {
+        for (const [set, cells] of row_state.each_set()) {
+          shuffle(cells)
+          cells.forEach((cell, i) => {
+            if (i == 0 || Math.random() < 0.33) {
               cell.link(cell.south)
               next_row.record(row_state.set_for(cell), cell.south)
             }
-          }
+          })
         }
         row_state = next_row
       }
